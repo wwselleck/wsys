834d5fbd6c459772a846ac4e906af4ed5f42d303
Weston Selleck <wwselleck@gmail.com>
2022-07-04 15:54:43 -0500

    vim: Update lsp configs

--- nvim/files/lua/nvim-lsp.lua
+++ nvim/files/lua/nvim-lsp.lua
@@ -12,8 +12,8 @@ local function on_attach(client)
     buf_keymap(0, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', keymap_opts)
     buf_keymap(0, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', keymap_opts)
     buf_keymap(0, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', keymap_opts)
-    buf_keymap(0, 'n', 'gn', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', keymap_opts)
-        buf_keymap(0, 'n', 'gp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', keymap_opts)
+    buf_keymap(0, 'n', 'gn', '<cmd>lua vim.diagnostic.goto_next()<cr>', keymap_opts)
+        buf_keymap(0, 'n', 'gp', '<cmd>lua vim.diagnostic.goto_prev()<cr>', keymap_opts)
     buf_keymap(0, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', keymap_opts)
     if client.resolved_capabilities.document_formatting then
         cmd('au BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 5000)')
@@ -76,28 +76,17 @@ local servers = {
             }
         }
 
-    }
+    },
+    jedi_language_server = {},
 }
 
+
+lsp_installer.setup({
+    automatic_installation = true
+})
+
 for server, config in pairs(servers) do
-    local server_available, requested_server = lsp_installer_servers.get_server(server)
-    if server_available then
-        requested_server:on_ready(function()
-            config.on_attach = config.on_attach or on_attach
-
-            -- (optional) Customize the options passed to the server
-            -- if server.name == "tsserver" then
-            --     opts.root_dir = function() ... end
-            -- end
-
-            -- This setup() function is exactly the same as lspconfig's setup function.
-            -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-            requested_server:setup(config)
-        end)
-
-        if not requested_server:is_installed() then
-            requested_server:install()
-        end
-    end
+    config.on_attach = config.on_attach or on_attach
+    lspconfig[server].setup(config)
 end
 
